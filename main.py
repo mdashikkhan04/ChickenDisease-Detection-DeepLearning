821a3db293b46013904c0881069515a3d4c1124b
Md. Ashikur Rahman Khan <ashik.est04@gmail.com>
2025-11-04 07:52:51 +0600

    data ingestion moduler code added in the main code base

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from cnnimgClassifier import logger
+from cnnimgClassifier.pipeline.Stage_01_Data_ingestion import DataIngestionTrainingPipeline
+from dotenv import load_dotenv
 
-logger.info("Strating the CNN Img Classifier application")
+load_dotenv()
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
